82f9455ac274cd33e428ccca0d0afc8597b6074d
Reginaldo Barreto <reginaldo.barreto@gmail.com>
2019-12-02 18:32:37 -0300

    efetuada programacao do conjunto TabAdapter, ViewPager e o TabLayout

--- app/src/main/java/br/com/barrsoft/tabbedsimply/MainActivity.java
+++ app/src/main/java/br/com/barrsoft/tabbedsimply/MainActivity.java
@@ -1,14 +1,32 @@
 package br.com.barrsoft.tabbedsimply;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
+import br.com.barrsoft.tabbedsimply.adapter.TabAdapter;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tabLayout)
+    TabLayout tabLayout;
+    @BindView(R.id.viewPager)
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        TabAdapter adapter = new TabAdapter(getSupportFragmentManager(),0, getResources().getStringArray(R.array.tabTitle));
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
