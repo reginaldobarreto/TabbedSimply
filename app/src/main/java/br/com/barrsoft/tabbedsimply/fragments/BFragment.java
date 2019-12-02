dbdd26414453a1e6c5d3317b0e78f6ca5a8958b6
Reginaldo Barreto <reginaldo.barreto@gmail.com>
2019-12-02 16:01:27 -0300

    adicionado os fragments

---
+++ app/src/main/java/br/com/barrsoft/tabbedsimply/fragments/BFragment.java
@@ -0,0 +1,32 @@
+package br.com.barrsoft.tabbedsimply.fragments;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import br.com.barrsoft.tabbedsimply.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BFragment extends Fragment {
+
+
+    public BFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_b, container, false);
+    }
+
+}
